18252e222e73aaa914a5b22578fd2df388a317d6
Pavel Arkhipov <pashko00710@yandex.ru>
2016-06-23 15:31:14 +0300

    Add support design library

--- app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
@@ -8,42 +8,52 @@ import com.softdesign.devintensive.R;
 import com.softdesign.devintensive.utils.ConstantManager;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String TAG = ConstantManager.TAG_PREFIX + "Main Activity";
+    public static final String TAG = ConstantManager.TAG_PREFIX + " Main Activity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
         setTitle(R.string.app_name_me);
         setContentView(R.layout.activity_main);
+        if(savedInstanceState == null) {
+
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d(TAG, "Выполнен метод onResume");
+        Log.d(TAG, "onResume");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d(TAG, "Выполнен метод onPause");
+        Log.d(TAG, "onPause");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d(TAG, "Выполнен метод onStop");
+        Log.d(TAG, "onStop");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.d(TAG, "Выполнен метод onRestart");
+        Log.d(TAG, "onRestart");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d(TAG, "Выполнен метод onDestroy");
+        Log.d(TAG, "onDestroy");
     }
 }
